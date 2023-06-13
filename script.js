fe0e098b384bab4393c885b88f673597996fddc5
precious1-2 <adekoyaprecious180@gmail.com>
2023-06-13 21:17:22 +0800

    duh

---
+++ script.js
@@ -0,0 +1,38 @@
+// // select eelement on the web page - class, id, tagname, css selector
+// // const heading = document.getElementByClassName("heading");
+// // console.log(heading);
+// // const para = document.getElementById("ptag")
+// // console.log(para);
+
+// // const p = document.getElementsByTagName("p")
+// // console.log(p);
+
+// // querySelector - css selector - class, #id, p
+// // querySelectorAll
+
+// const paragragh = document.querySelector("p");
+// console.log(paragragh);
+// const paragraghs = document.querySelectorAll("p");
+// paragraghs.forEach((p) => console.log(p));
+// const h1 = document.querySelector(".heading");
+// console.log(h1);
+
+// const div = document.querySelector("#test")
+// console.log(div);
+
+// // changing content on the web - inner text, innerContent, innerHtml
+// console.log(h1.innerText);
+// h1.innerText += "welcome"
+// console.log(h1.textContent);
+// h1.textContent = "hello class"
+// console.log(div.innerHTML);
+// // div.innerHtml += "<h1>FROM JS </h1>"
+// div.innerHTML += "<a>visit google</a>";
+
+// const anotherH = document.querySelector("#test h1")
+
+// const para = document.querySelector("body")
+// para.innerHTML = '<h1>JAVA SCRIPT</h1>'
+// para.style.backgroundColor = "blue"
+
+// responding to users interaction
